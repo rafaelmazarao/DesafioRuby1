cedab5ffc9cc622920edd4b10585cb8f1df0f1f7
Rafael Mazarao <rafaelmazarao@hotmail.com>
2024-07-20 11:54:25 -0300

    Desafio concluido

---
+++ Desafio1.rb
@@ -0,0 +1,7 @@
+print "Digite seu nome: "
+nome = gets.chomp
+print "Digite seu sobrenome: "
+sobrenome = gets.chomp
+print "Digite sua idade: "
+idade = gets.chomp
+puts "Ola #{nome} #{sobrenome}! Seja bem vindo! Sua idade e #{idade}."
